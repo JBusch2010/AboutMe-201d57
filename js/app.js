673bd7fa447411f96a53cf321e04a166f66c914f
Joachen D Busch <joachendbusch@gmail.com>
2019-06-04 22:28:48 -0700

    Push for assignment

---
+++ js/app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+var firstname = prompt('Did you know my first name is German?');
+
+if(firstname === 'yes' || firstname === 'y' || firstname === 'Yes'){
+  alert('Oh.. well look at you! being great!');
+} else if(firstname === 'no' || firstname === 'n' || firstname === 'No'){
+  alert('Well, now you know!');
+} else {
+  alert('you didnt even try');
+}
+console.log(firstname + ' -Response to, Did you know my first name is German');
+
+var skateboard = prompt('Do you think I skateboard?');
+
+if(skateboard === 'yes' || skateboard === 'y' || skateboard === 'Yes'){
+  alert('*does kickflip on your desk* You are right!');
+} else if(skateboard === 'no' || skateboard === 'n' || skateboard === 'No'){
+  alert('Welp... *does double kickflip on your desk* I do!');
+} else {
+  alert('did you even try...?');
+}
+console.log(skateboard + ' -Response to, Do you think I skateboard');
+
+var gummybears = prompt('Did you know my favorite candy is gummy bears?');
+
+if(gummybears === 'yes' || gummybears === 'y' || skateboard === 'Yes'){
+  alert('Good guess! I shall share them with you!');
+} else if(gummybears === 'no' || gummybears === 'n' || gummybears === 'No'){
+  alert('Welp no gummybears for you then!');
+} else {
+  alert('you didnt even try.. you DEFINITELL aint gettin any gummybears');
+}
+console.log(gummybears + ' -Response to, Did you know my favorite candy is gummybears?');
+
+var Batman = prompt('Would you say my favorite superhero is Batman?');
+
+if(Batman === 'yes' || Batman === 'y' || Batman === 'Yes'){
+  alert('Good. This is the only answer to this question.');
+} else if(Batman === 'no' || Batman === 'n' || Batman === 'No'){
+  alert('You meant, yes. But thats okay!');
+} else {
+  alert('Why you aint try? To arkham with you!');
+}
+console.log(Batman + ' -Response to, Would you say my favorite superhero is Batman?');
+
+var georgia = prompt('Did you know that I am from georgia?');
+
+if(georgia === 'yes' || georgia === 'y' || georgia === 'Yes'){
+  alert('You a real one!');
+} else if( georgia === 'no' || georgia === 'n' || georgia === 'No'){
+  alert('Well, now you know!');
+} else {
+  alert('did you even try...?');
+}
+console.log(georgia + ' -Response to, Did you know that I am from georgia');
+
+
